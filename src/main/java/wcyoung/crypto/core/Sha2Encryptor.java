97022f5fd2aa221dc2b23773ec5a140b12a216d1
Chiyoung Won <wcyoung98@gmail.com>
2020-11-30 23:57:19 +0900

    SHA2 Encryptor 추가

---
+++ src/main/java/wcyoung/crypto/core/Sha2Encryptor.java
@@ -0,0 +1,41 @@
+package wcyoung.crypto.core;
+
+import wcyoung.crypto.codec.Coder;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Sha2Encryptor {
+
+    public static final String SHA_256 = "SHA-256";
+    public static final String SHA_512 = "SHA-512";
+
+    private final Coder coder;
+
+    public Sha2Encryptor() {
+        this(DefaultCryptoConst.CODER);
+    }
+
+    public Sha2Encryptor(Coder coder) {
+        this.coder = coder;
+    }
+
+    public String encrypt256(String plainText) {
+        return encrypt(plainText, SHA_256);
+    }
+
+    public String encrypt512(String plainText) {
+        return encrypt(plainText, SHA_512);
+    }
+
+    public String encrypt(String plainText, String algorithm) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance(algorithm);
+            digest.update(plainText.getBytes());
+            return coder.encode(digest.digest());
+        } catch (NoSuchAlgorithmException e) {
+            throw new CryptoRuntimeException(e);
+        }
+    }
+
+}
---
+++ src/test/java/wcyoung/crypto/core/Sha2EncryptorTest.java
@@ -0,0 +1,37 @@
+package wcyoung.crypto.core;
+
+import org.junit.jupiter.api.Test;
+import wcyoung.crypto.codec.Coder;
+import wcyoung.crypto.codec.Coders;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Sha2EncryptorTest {
+
+    private final Coder CODER = Coders.hex();
+    private final String PLAIN_TEXT = "sha2 encrypt test text!!";
+    private final String CIPHER_TEXT_256 = "85e5adf29aa42b5138238eebbbbdd2e6ffd1431590146be4d8dd81c3656634b2";
+    private final String CIPHER_TEXT_512 = "6daf2dc3bb93a9462d79aac07565647556d100bc498247bc576e48cd65576a209b9c8f48785df513d423fcd7570de1c48d46942555c0bf02337c96ecb4a37a0b";
+
+    @Test
+    public void testEncrypt256() {
+        Sha2Encryptor encryptor = new Sha2Encryptor(CODER);
+        String cipherText = encryptor.encrypt256(PLAIN_TEXT);
+        assertEquals(cipherText, CIPHER_TEXT_256);
+    }
+
+    @Test
+    public void testEncrypt512() {
+        Sha2Encryptor encryptor = new Sha2Encryptor(CODER);
+        String cipherText = encryptor.encrypt512(PLAIN_TEXT);
+        assertEquals(cipherText, CIPHER_TEXT_512);
+    }
+
+    @Test
+    public void testEncrypt() {
+        Sha2Encryptor encryptor = new Sha2Encryptor(CODER);
+        String cipherText = encryptor.encrypt(PLAIN_TEXT, Sha2Encryptor.SHA_256);
+        assertEquals(cipherText, CIPHER_TEXT_256);
+    }
+
+}
