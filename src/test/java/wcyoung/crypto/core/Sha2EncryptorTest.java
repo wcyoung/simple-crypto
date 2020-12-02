6b78a752044140ca94af81b371496d94910b4b06
Chiyoung Won <wcyoung98@gmail.com>
2020-12-03 00:21:32 +0900

    Sha2EncryptorTest 메소드명 수정

--- src/test/java/wcyoung/crypto/core/Sha2EncryptorTest.java
+++ src/test/java/wcyoung/crypto/core/Sha2EncryptorTest.java
@@ -14,21 +14,21 @@ public class Sha2EncryptorTest {
     private final String CIPHER_TEXT_512 = "6daf2dc3bb93a9462d79aac07565647556d100bc498247bc576e48cd65576a209b9c8f48785df513d423fcd7570de1c48d46942555c0bf02337c96ecb4a37a0b";
 
     @Test
-    public void testEncrypt256() {
+    public void encrypt256() {
         Sha2Encryptor encryptor = new Sha2Encryptor(CODER);
         String cipherText = encryptor.encrypt256(PLAIN_TEXT);
         assertEquals(cipherText, CIPHER_TEXT_256);
     }
 
     @Test
-    public void testEncrypt512() {
+    public void encrypt512() {
         Sha2Encryptor encryptor = new Sha2Encryptor(CODER);
         String cipherText = encryptor.encrypt512(PLAIN_TEXT);
         assertEquals(cipherText, CIPHER_TEXT_512);
     }
 
     @Test
-    public void testEncrypt() {
+    public void encrypt() {
         Sha2Encryptor encryptor = new Sha2Encryptor(CODER);
         String cipherText = encryptor.encrypt(PLAIN_TEXT, Sha2Encryptor.SHA_256);
         assertEquals(cipherText, CIPHER_TEXT_256);
