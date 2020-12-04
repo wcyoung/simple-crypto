e8e6ceea32217850304be7271e11c9efde2a83c9
Chiyoung Won <wcyoung98@gmail.com>
2020-12-04 22:34:43 +0900

    Base64 Encoder padding 설정 기능 추가

--- src/main/java/wcyoung/crypto/codec/Base64.java
+++ src/main/java/wcyoung/crypto/codec/Base64.java
@@ -2,9 +2,23 @@ package wcyoung.crypto.codec;
 
 public class Base64 implements Coder {
 
+    private final boolean doPadding;
+
+    public Base64() {
+        this(true);
+    }
+
+    public Base64(boolean doPadding) {
+        this.doPadding = doPadding;
+    }
+
     @Override
     public String encode(byte[] bytes) {
-        return java.util.Base64.getEncoder().encodeToString(bytes);
+        java.util.Base64.Encoder encoder = java.util.Base64.getEncoder();
+        if (!doPadding) {
+            encoder = encoder.withoutPadding();
+        }
+        return encoder.encodeToString(bytes);
     }
 
     @Override
--- src/main/java/wcyoung/crypto/codec/Base64Url.java
+++ src/main/java/wcyoung/crypto/codec/Base64Url.java
@@ -1,10 +1,26 @@
 package wcyoung.crypto.codec;
 
+import java.util.Base64;
+
 public class Base64Url implements Coder {
+
+    private final boolean doPadding;
+
+    public Base64Url() {
+        this(true);
+    }
+
+    public Base64Url(boolean doPadding) {
+        this.doPadding = doPadding;
+    }
 
     @Override
     public String encode(byte[] bytes) {
-        return java.util.Base64.getUrlEncoder().encodeToString(bytes);
+        Base64.Encoder encoder = java.util.Base64.getUrlEncoder();
+        if (!doPadding) {
+            encoder = encoder.withoutPadding();
+        }
+        return encoder.encodeToString(bytes);
     }
 
     @Override
